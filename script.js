25e2c5211d2bcdc67c25be899d57c1e61f1afdc7
Samit1104 <166905415+Samit1104@users.noreply.github.com>
2025-11-11 21:21:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function removeColor() {
+      // Get the dropdown element
+      const select = document.getElementById("colorSelect");
+
+      // Get the selected index
+      const selectedIndex = select.selectedIndex;
+
+      // Check if an option is selected
+      if (selectedIndex !== -1) {
+        // Remove the selected option
+        select.remove(selectedIndex);
+      } else {
+        alert("Please select a color to remove!");
+      }
+    }
